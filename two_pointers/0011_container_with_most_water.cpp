ed65c8d72bf985534bd2f0b7e720ee485676f4fb
Shaaswat K Jha <shaaswat.dev@proton.me>
2026-09-23 22:28:59 +0530

    fixed potential underflow issue

--- two_pointers/0011_container_with_most_water.cpp
+++ two_pointers/0011_container_with_most_water.cpp
@@ -8,7 +8,7 @@ public:
         int result = 0;
 
         int left = 0;
-        int right = static_cast<int>(height.size() - 1);
+        int right = static_cast<int>(height.size()) - 1;
 
         while (left < right) {
             int area = (right - left) * std::min(height[left], height[right]);
